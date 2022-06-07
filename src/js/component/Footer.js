57fb82355750507bf625d5631eeed6fa92a11b50
NChang007 <nelsonc924@gmail.com>
2022-06-07 00:26:50 +0000

    Should be okay

---
+++ src/js/component/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+	return (
+        <div className="footer">
+            <div style={{color: 'white'}}>
+                <p>Copyright something something</p>
+            </div>
+        </div>
+		
+	);
+};
+
+export default Footer;
---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+	return (
+	
+			<div className="jumbotron">
+				<h1>A Warm Welcome</h1>
+				<p>Bacon ipsum dolor amet porchetta cow pork loin short ribs. Pig landjaeger pork chop jowl kielbasa, drumstick pancetta picanha doner t-bone sausage salami. Burgdoggen bresaola fatback swine kevin pig. Jowl pig shank chicken tail short ribs, landjaeger pork belly short loin flank meatloaf. Beef ribs shankle swine t-bone pork chop prosciutto pastrami, ham leberkas bacon fatback hamburger spare ribs meatloaf. Flank ham turducken, ground round pork chop filet mignon sirloin hamburger ribeye capicola salami.</p>
+				<button type="button" className="btn btn-primary" href="https://baconipsum.com/">Try Lorem Bacon</button>
+			</div>
+		
+	);
+};
+
+export default Jumbotron;
